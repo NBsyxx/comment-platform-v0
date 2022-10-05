90e36c037270b9d453cf5d31fd276ad9504e3bb2
Yunxiao Sg <yunxiao.song@hotmail.com>
2022-10-05 15:52:07 -0500

    spingboot future for page generations

---
+++ commenterdemo/src/test/java/com/commenter/commenterdemo/CommenterdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commenter.commenterdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommenterdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
